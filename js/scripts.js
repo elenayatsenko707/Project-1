fd1fbcb837b3a21ae1cc7105a692766bf97c08b0
elenayatsenko707 <lenasova95@gmil.com>
2021-04-05 14:09:09 +0300

    lesson14

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
     $(".mobile-button").on("click",function(){
         $(".navigation").slideToggle();
         $(this).toggleClass("opened");
-    })
+    });
+    
+    $(function() {
+  
+        $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+          $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+        
+      });
 
 });
